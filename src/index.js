bff8a8f8bd00fd493ce2adfe67834ff5e4c70b47
aguvai <145935152+aguvai@users.noreply.github.com>
2025-07-12 17:18:50 -0400

    Add logic to clear page on navbar button click

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import "./styles.css";
 import loadHomePage from "./homepage"
 
+const contentDiv = document.querySelector("#content");
+
+const navbar = document.querySelector("nav");
+
+const clearPage = function() {
+    while (contentDiv.hasChildNodes()) {
+        contentDiv.firstChild.remove();
+    }
+}
+
+const buttons = navbar.querySelectorAll("button");
+
+for (let button of buttons) {
+    button.addEventListener("click", (event) => {
+        clearPage();
+    })
+}
+
 loadHomePage();
