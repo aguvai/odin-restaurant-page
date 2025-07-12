be7d296989fe605d3f77c2f33ce3671f59d08224
aguvai <145935152+aguvai@users.noreply.github.com>
2025-07-12 16:07:36 -0400

    Add attribute setting capacity to createElement.js
    Added a new object to be passed through elementProperties that contains
    an element's attributes. This makes iframe elements (for Google Maps
    embeds) to be compatible with createElement.js.

--- src/createElement.js
+++ src/createElement.js
@@ -1,13 +1,21 @@
 const createElement = function (elementProperties) {
-    // elementProperties = {element's class/id, element's textContent}
-
     const element = document.createElement(elementProperties.elementType);
+   
     if (elementProperties.elementClass) {
         element.classList.add(elementProperties.elementClass);
-    }
+    };
+
     if (elementProperties.elementTextContent) {
         element.textContent = elementProperties.elementTextContent;
-    }
+    };
+
+    if (elementProperties.elementAttributes) {
+        let attributes = elementProperties.elementAttributes;
+        for (let attribute in attributes) {
+            element.setAttribute(attribute, attributes[attribute])
+        }
+    };
+
     elementProperties.appendTo.appendChild(element);
 
     return element;
--- src/homepage.js
+++ src/homepage.js
@@ -2,11 +2,11 @@ const contentDiv = document.querySelector("#content");
 
 import { createElement } from "./createElement";
 
-const createDivider = function(appendTo, type) {
+const createDivider = function(appendTo, divType) {
     let divider = createElement({
         elementType: "div",
         appendTo: appendTo,
-        elementClass: type,
+        elementClass: divType,
     });
 };
 
@@ -76,7 +76,46 @@ const createInfoContainer = function() {
     createDivider(infoContainer, "info-divider");
 };
 
+const createVisitUsContainer = function(){
+    let visitUsContainer = createElement({
+        elementType: "div",
+        appendTo: contentDiv,
+        elementClass: "visit-us",
+    })
+
+    let visitUsHeader = createElement({
+        elementType: "h1",
+        appendTo: visitUsContainer,
+        elementTextContent: "Visit Us"
+    })
+
+    createDivider(visitUsContainer, "info-divider")
+
+    let locationContainer = createElement({
+        elementType: "div",
+        appendTo: visitUsContainer,
+        elementClass: "location-container",
+    })
+
+    let map = createElement({
+        elementType: "iframe",
+        appendTo: locationContainer,
+        elementAttributes: {
+            src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.8356633773506!2d-73.98531572338615!3d40.7656390713857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258f7fcad6cdb%3A0xa98fe24a9c1cfe15!2sPatsy's%20Italian%20Restaurant!5e0!3m2!1sen!2sus!4v1752286358886!5m2!1sen!2sus",
+            width: "400",
+            height: "300",
+            style: "border:0;",
+            allowfullscreen: "",
+            loading: "lazy",
+            referrerpolicy: "no-referrer-when-downgrade",
+        }
+    })
+}
+
 export default function() {
     createBanner();
+
     createInfoContainer();
+
+    createVisitUsContainer();
 }
