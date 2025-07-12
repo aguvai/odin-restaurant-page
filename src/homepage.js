b407d5de6d8c30f873109d12bde1e87ba5bb7da6
aguvai <145935152+aguvai@users.noreply.github.com>
2025-07-12 13:05:18 -0400

    Finish banner DOM creation in homepage.js

--- src/homepage.js
+++ src/homepage.js
@@ -2,17 +2,31 @@ const contentDiv = document.querySelector("#content");
 
 import {createElement} from "./createElement";
 
+function createDivider(banner) {
+    let divider = createElement(
+        "div", banner, {elementIdentifier: "banner-divider"}
+    );
+}
+
 function createBanner() {
     let banner = createElement(
         "div", contentDiv, {elementIdentifier: "banner"}
     );
 
-    let divider = createElement(
-        "div", banner, {elementIdentifier: "banner-divider"}
-    );
+    createDivider(banner);
 
     let restarauntName = createElement(
         "h1", banner, {elementTextContent: "Patsy's Italian Restaurant"}
+    );
+
+    let restarauntSubtext = createElement(
+        "h2", banner, {elementTextContent: "A New York landmark made famous by Frank Sinatra"}
+    );
+
+    createDivider(banner);
+
+    let menuButton = createElement(
+        "button", banner, {elementTextContent: "VIEW ALL DAY MENU"}
     );
 }
 
